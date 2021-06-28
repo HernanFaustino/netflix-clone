7d456cd94d7ce367dd9f8d9ffa4f92ed7a0dd401
Hernan <hernan@2ulaundry.com>
2021-06-27 23:15:31 -0500

    Initial frontend for project

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDiICQETx-H_x8zGlMq2M97IXGQS-KB6sw",
+  authDomain: "netflix-build-803d6.firebaseapp.com",
+  projectId: "netflix-build-803d6",
+  storageBucket: "netflix-build-803d6.appspot.com",
+  messagingSenderId: "1075368429464",
+  appId: "1:1075368429464:web:89a41218b7d4d5b6744ded"
+};
